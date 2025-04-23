e0ecc3ff37948bdad97f46466a73488986ae08a8
code-woncheol <codewc23@gmail.com>
2025-04-23 15:48:11 +0900

    오탈자 수정
    오탈자 수정

--- WorkEasy/src/App.tsx
+++ WorkEasy/src/App.tsx
@@ -1,16 +1,15 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserHomePage from './pages/user/UserHomePage';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserHomePage from "./pages/worker/UserHomePage";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/UserHomePage" element={<UserHomePage />} />
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/UserHomePage" element={<UserHomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
